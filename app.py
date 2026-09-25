c312f4af1bf8a3c21c2c00b5bb7c4fd84c1dd165
mazex-123 <ahsanymstfy843@gmail.com>
2026-09-25 12:01:04 +0330

    Fix production session cookies

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ import os
 app = Flask(__name__)
 app.secret_key = os.environ.get("NOVA_SECRET_KEY")
 
+app.config["SESSION_COOKIE_SAMESITE"] = "None"
+app.config["SESSION_COOKIE_SECURE"] = True
+
 CORS(
     app,
     supports_credentials=True,
