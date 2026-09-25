8d2ca32a3daae8537332fbeea08c72da8e83bb35
mazex-123 <ahsanymstfy843@gmail.com>
2026-09-25 15:27:55 +0330

    Allow Cloudflare Pages origin

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ CORS(
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "https://approaches-olive-earthquake-renew.trycloudflare.com"
+        "https://nova-social-60a.pages.dev"
     ]
 )
 
